d2cf4bab716272cad63980a9ea7b73133c068dc0
tysdz <thanhtenty@gmail.com>
2023-12-11 15:07:15 +0700

    nothing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React, { Fragment } from "react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from "./redux/add.tsx";
+import store from "./redux/add";
 import './App.scss';
-import Header from './header/header.tsx';
-import Footer from './footer/footer.tsx';
-import HomePage from './pages/HomePage.tsx';
-import NEUPage from "./pages/NEUpage.tsx";
-import ScrollingBar from "./header/ScrollingBar.tsx";
-import CartPage from "./pages/CartPage.tsx";
+import Header from './header/header';
+import Footer from './footer/footer';
+import HomePage from './pages/HomePage';
+import NEUPage from "./pages/NEUpage";
+import ScrollingBar from "./header/ScrollingBar";
+import CartPage from "./pages/CartPage";
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -14,4 +14,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log); // Change to your desired reporting function or remove if not needed
